6e20fecd64609b9a9e2a225c13f1d30dcd184066
Katy DeCorah <kdecorah@gmail.com>
2013-09-12 13:17:17 -0400

    consolidated js for < 800 width

--- js/main.js
+++ js/main.js
@@ -146,10 +146,14 @@ $(".history").click(function(){
   }
 });
 
-// Bootstrap Tooltips
-if ($(window).width() > 500) {
+// Bootstrap Tooltips & Twitter not for mobile-ish
+if ($(window).width() > 800) {
   $('[data-rel=tooltip]').tooltip({ trigger: "hover", placement:"bottom" });
+
+  !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+
 }
+
 
 
 
