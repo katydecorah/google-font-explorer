6879a841f8de7a76b87776b9b5704d9c094425e2
Katy DeCorah <katydecorah@gmail.com>
2020-06-06 10:34:16 -0400

    :wrench: update dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,4 @@
-module.exports = function(grunt) {
-
+module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
@@ -8,7 +7,11 @@ module.exports = function(grunt) {
         separator: ';',
       },
       dist: {
-        src: ['js/lib/bootstrap-dropdown.js', 'js/lib/bootstrap-typeahead.js','js/lib/app.js'],
+        src: [
+          'js/lib/bootstrap-dropdown.js',
+          'js/lib/bootstrap-typeahead.js',
+          'js/lib/app.js',
+        ],
         dest: 'js/lib/main.js',
       },
     },
@@ -16,52 +19,56 @@ module.exports = function(grunt) {
     uglify: {
       build: {
         src: 'js/lib/main.js',
-        dest: 'js/main.min.js'
-      }
+        dest: 'js/main.min.js',
+      },
     },
 
     sass: {
       dist: {
         options: {
-          style: 'compressed'
+          style: 'compressed',
         },
         files: {
-          'css/style.css': 'css/lib/style.scss'
-        }
-      }
+          'css/style.css': 'css/lib/style.scss',
+        },
+      },
     },
 
     autoprefixer: {
       options: {
-        browsers: ['last 2 version', 'ie 9']
+        browsers: ['last 2 version', 'ie 9'],
       },
       target: {
-        src: 'css/style.css'
-      }
+        src: 'css/style.css',
+      },
     },
     watch: {
       scripts: {
         files: ['js/lib/*.js'],
-        tasks: ['concat','uglify'],
+        tasks: ['concat', 'uglify'],
         options: {
           spawn: false,
         },
       },
       css: {
         files: ['css/lib/*.scss'],
-        tasks: ['sass','autoprefixer'],
+        tasks: ['sass', 'autoprefixer'],
         options: {
           spawn: false,
-        }
-      }
-    }
-
+        },
+      },
+    },
   });
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-autoprefixer');
-  grunt.registerTask('default', ['concat','uglify','sass','autoprefixer','watch']);
-
+  grunt.registerTask('default', [
+    'concat',
+    'uglify',
+    'sass',
+    'autoprefixer',
+    'watch',
+  ]);
 };
--- js/lib/main.js
+++ js/lib/main.js
@@ -502,128 +502,165 @@
   })
 
 }(window.jQuery);
-;var url ="https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyDzNP2S2VIo_jiErF8L--b6V8bkaKoQNQg";
+;var url =
+  'https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyDzNP2S2VIo_jiErF8L--b6V8bkaKoQNQg';
 
-var family = "";
+var family = '';
 runFont(family);
 var families = [];
 var visited = [];
 
-
-$.getJSON(url,function(json){
-
+$.getJSON(url, function (json) {
   // counts all the families
   var countFamilies = json.items.length;
-  $("#count").text(countFamilies);
+  $('#count').text(countFamilies);
 
   // pushes all the font families to an array
-  $.each(json.items,function(i,type){
+  $.each(json.items, function (i, type) {
     families.push(type.family);
   });
 });
 
-
-
 // Bootstrap Typeahead
 $('#font-search').typeahead({
   source: families,
-  updater:function (item) {
+  updater: function (item) {
     runFont(item);
-    $("#font-search").attr("placeholder",item);
-  }
+    $('#font-search').attr('placeholder', item);
+  },
 });
 
 // Gets the font based on family name
 function runFont(family) {
-  $.getJSON(url,function(json){
-    $.each(json.items,function(i,type){
+  $.getJSON(url, function (json) {
+    $.each(json.items, function (i, type) {
       if (type.family === family) {
         var familyPlus = family.replace(/\s/g, '+');
-        var familyCSS = "https://fonts.googleapis.com/css?family=" + familyPlus + ":" + type.variants + "";
-        var details = $("#variants");
+        var familyCSS =
+          'https://fonts.googleapis.com/css?family=' +
+          familyPlus +
+          ':' +
+          type.variants +
+          '';
+        var details = $('#variants');
 
         // Removes previous family and style
-        $(".style").remove();
-        $("style").remove();
+        $('.style').remove();
+        $('style').remove();
         details.empty();
 
         // Grabs family details
-        details.append("<div class='detail-title'>Variants</div><ul class='list-variant'></ul>");
-        $(type.variants).each(function(index, item) {
-          $(".list-variant").append(
-            $(document.createElement('li')).text(item)
-          );
+        details.append(
+          "<div class='detail-title'>Variants</div><ul class='list-variant'></ul>"
+        );
+        $(type.variants).each(function (index, item) {
+          $('.list-variant').append($(document.createElement('li')).text(item));
         });
-        details.append("<div class='detail-title'>Subsets</div><ul class='list-subsets'></ul>");
-        $(type.subsets).each(function(index, item) {
-          $(".list-subsets").append(
-            $(document.createElement('li')).text(item)
-          );
+        details.append(
+          "<div class='detail-title'>Subsets</div><ul class='list-subsets'></ul>"
+        );
+        $(type.subsets).each(function (index, item) {
+          $('.list-subsets').append($(document.createElement('li')).text(item));
         });
-        details.append("<div class='detail-title horizontal'>Category</div><div class='detail-data'>" + type.category + "</div>");
-        details.append("<div class='detail-title horizontal'>Version</div><div class='detail-data'>" + type.version + "</div>");
-        details.append("<div class='detail-title horizontal'>Last Modified</div><div class='detail-data'>" + type.lastModified + "</div>");
-        details.append("<div class='detail-title'>HTML</div><div class='detail-data'><pre><code class='language-markup'>&lt;link href='"+familyCSS+"' rel='stylesheet' type='text/css'&gt;</code></pre></div>");
-        details.append("<div class='detail-title'>CSS</div><div class='detail-data'><pre><code class='language-css'>font-family: '"+family+"', sans-serif;</code></pre></div>");
-        details.append("<a href='https://fonts.google.com/specimen/"+familyPlus+"' class='btn'>View on Google Fonts &rarr;</a>");
+        details.append(
+          "<div class='detail-title horizontal'>Category</div><div class='detail-data'>" +
+            type.category +
+            '</div>'
+        );
+        details.append(
+          "<div class='detail-title horizontal'>Version</div><div class='detail-data'>" +
+            type.version +
+            '</div>'
+        );
+        details.append(
+          "<div class='detail-title horizontal'>Last Modified</div><div class='detail-data'>" +
+            type.lastModified +
+            '</div>'
+        );
+        details.append(
+          "<div class='detail-title'>HTML</div><div class='detail-data'><pre><code class='language-markup'>&lt;link href='" +
+            familyCSS +
+            "' rel='stylesheet' type='text/css'&gt;</code></pre></div>"
+        );
+        details.append(
+          "<div class='detail-title'>CSS</div><div class='detail-data'><pre><code class='language-css'>font-family: '" +
+            family +
+            "', sans-serif;</code></pre></div>"
+        );
+        details.append(
+          "<a href='https://fonts.google.com/specimen/" +
+            familyPlus +
+            "' class='btn'>View on Google Fonts &rarr;</a>"
+        );
 
         // Grabs the Google Font
-        $("head").append("<link href='"+ familyCSS +"' rel='stylesheet' type='text/css' class='style'>");
-        $("body").css("font-family",family);
+        $('head').append(
+          "<link href='" +
+            familyCSS +
+            "' rel='stylesheet' type='text/css' class='style'>"
+        );
+        $('body').css('font-family', family);
 
         // If family has italic or bold, allow it
-        if(details.text().match('italic')){
-          $("head").append("<style>.main em { font-style: italic; }");
+        if (details.text().match('italic')) {
+          $('head').append('<style>.main em { font-style: italic; }');
         }
-        if(family.match('SC')){
-          $("head").append("<style>.main { font-variant: small-caps; }");
+        if (family.match('SC')) {
+          $('head').append('<style>.main { font-variant: small-caps; }');
         }
 
-        if(details.text().match('700')){
-          $("head").append("<style>.main strong,h1,h2,h3 { font-weight: 700; }");
-        }
-        else if(details.text().match('800')){
-          $("head").append("<style>.main strong,h1,h2,h3 { font-weight: 800; }");
-        }
-        else if(details.text().match('900')){
-          $("head").append("<style>.main strong,h1,h2,h3 { font-weight: 900; }");
+        if (details.text().match('700')) {
+          $('head').append(
+            '<style>.main strong,h1,h2,h3 { font-weight: 700; }'
+          );
+        } else if (details.text().match('800')) {
+          $('head').append(
+            '<style>.main strong,h1,h2,h3 { font-weight: 800; }'
+          );
+        } else if (details.text().match('900')) {
+          $('head').append(
+            '<style>.main strong,h1,h2,h3 { font-weight: 900; }'
+          );
         }
 
         // Save visited families
-        visited.push( family );
-        var visit="";
-        $.each(visited, function(i, val) {
-          visit+= "<li class='link-history' data-family='"+ val +"'>" + val + "</li>";
+        visited.push(family);
+        var visit = '';
+        $.each(visited, function (i, val) {
+          visit +=
+            "<li class='link-history' data-family='" +
+            val +
+            "'>" +
+            val +
+            '</li>';
         });
         $('#visited').html(visit);
 
         // Creates link to view that font again
-        $(".link-history").click(function(){
-          var dat = $(this).attr("data-family");
+        $('.link-history').click(function () {
+          var dat = $(this).attr('data-family');
           runFont(dat);
-          $("#font-search").attr("placeholder",dat);
+          $('#font-search').attr('placeholder', dat);
 
           // remove first instance from array
           if ($.inArray(dat, visited) !== -1) {
-            visited.splice( $.inArray(dat, visited), 1 );
+            visited.splice($.inArray(dat, visited), 1);
           }
         });
-
       }
     });
   });
 }
 
-
 // Gets a random font
 function random() {
-  $.getJSON(url,function(json){
+  $.getJSON(url, function (json) {
     var count = json.items.length,
-    random = Math.ceil(Math.random() * count);
-    $.each(json.items,function(i,type){
-      if(i === random){
+      random = Math.ceil(Math.random() * count);
+    $.each(json.items, function (i, type) {
+      if (i === random) {
         var family = type.family;
-        $("#font-search").attr("placeholder", family);
+        $('#font-search').attr('placeholder', family);
         runFont(family);
       }
     });
@@ -631,7 +668,7 @@ function random() {
 }
 
 // Picks a random font on click
-$(".random").click(function(){
+$('.random').click(function () {
   random();
 });
 
